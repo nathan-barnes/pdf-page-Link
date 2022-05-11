2050e15b8aa1328f59e9d48d52cb255bb48eece3
Nathan Barnes <nbarnes@azahner.com>
2022-05-10 23:23:17 -0500

    commit working text spell check

--- linkPages.py
+++ linkPages.py
@@ -10,6 +10,8 @@ import json
 from spellchecker import SpellChecker
 
 import csv
+import itertools
+import re
 
 pdfLink = r"SCOTTSDALE_ZAHNER_100CD_SET_V3.pdf"
 
@@ -33,7 +35,8 @@ print(f'x1, x2: {x1, x2}\ny1, y2: {y1,y2}')
 print("page count is {0}".format(doc.page_count))
 print("page entity is ", doc)
 
-SearchText = ['XC-A501','XC-A502','XC-A503','XC-A504','XC-A505', 'XC-D501', 'XC-D502']
+SearchText = ['XC-001','XC-A101','XC-D101', 'XC-A201', 'XC-A501','XC-A502','XC-A503','XC-A504','XC-A505','XC-A506', 'XC-D501', 'XC-D502']
+excludeList = ['www.azahner.com', 'WWW.AZAHNER.COM', 'PURLIN', 'ZAHNER', 'AS-BUILT', 'ZEPP', 'Z-CLIP', 'TYP.','TYP', 'JAMBS', 'KERFED', 'LAKEFLATO', '24X36"', 'DG04']
 callouts = []
 details = []
 
@@ -52,10 +55,10 @@ def foundDetail (page, searchTxt):
     for w in wlist:  # scan through all words on page
         if searchTxt in w[4]:  # w[4] is the word's string
             if (w[0] < width and w[1] < height):
-                print ('found')
+                # print ('found')
                 r = fitz.Rect(w[:4])  # make rect from word bbox
                 rect.append(r)
-                print (w)
+                # print (w)
                 # page.add_underline_annot(r)  # underline
     
     return page.number, rect
@@ -76,8 +79,8 @@ def getDetailPage(page, searchTxt):
     for w in wlist:  # scan through all words on page
         if searchTxt in w[4]:  # w[4] is the word's string
             if (w[0] > width and w[1] > height):
-                print ('found page')
-                print (width, height, page.number )
+                # print ('found page')
+                # print (width, height, page.number )
                 # r = fitz.Rect(w[:4])  # make rect from word bbox
                 pageNumber = page.number
                 # rect.append(r)
@@ -95,9 +98,9 @@ def notFound():
 
 
 
-def writeCsv(pathToFile, header, data, encoding='UTF8', newline=''):
+def writeCsv(pathToFile, header, data):
     # open the file in the write mode
-    with open(pathToFile, 'w') as f:
+    with open(pathToFile, 'w', encoding='UTF8', newline='') as f:
         # create the csv writer
         writer = csv.writer(f)
         # write the header
@@ -105,6 +108,8 @@ def writeCsv(pathToFile, header, data, encoding='UTF8', newline=''):
 
         # write multiple rows
         writer.writerows(data)
+        # for d in data:
+        #     writer.writerow(d)
 
 
 ## -----------------------end functions -----------------------
@@ -118,7 +123,7 @@ for detailName in SearchText:
     detailLinkOkj[detailName] = {'toPages':[], 'destPage':[]}
     # detailLinkOkj[detailName]['pages'] = []
     
-print ('no idea', detailLinkOkj)
+# print ('no idea', detailLinkOkj)
 for page in doc:
     print('-----------page', page)
     
@@ -128,14 +133,14 @@ for page in doc:
         detpageNumber = getDetailPage(page, detailName)
         
         if (detpageNumber != -1):
-            pages.append(detpageNumber)
+            # pages.append(detpageNumber)
             detailLinkOkj[detailName]['toPages'].append(detpageNumber)
 
         # [pgNum, detailRect ]= foundDetail(page, "XC-D501")
         [pgNum, detailRect ]= foundDetail(page, detailName)
 
         if(len(detailRect)>0):
-            detials.append({pgNum: detailRect})
+            # detials.append({pgNum: detailRect})
             detailLinkOkj[detailName]['destPage'].append({pgNum: detailRect})
     
 # print ('no idea', detailLinkOkj)
@@ -154,48 +159,27 @@ for eachDetailObj in detailLinkOkj:
             detailRect = list(eachRect.values())[0]
             # print('detailPage',detailRect)
 
-            if (detailPage != pages[0]):
-                # print('detailRect', detailRect)
-                lnks = doc[detailPage].links()
-                # print('links', lnks)
+            # if (detailPage != pages[0]):
+            # print('detailRect', detailRect)
+            lnks = doc[detailPage].links()
+            # print('links', lnks)
 
-                for everyRect in detailRect:
-                    pageToLink = doc[detailPage]
-                    # print('pageToLink', pageToLink)
-                    print ('about to link', eachDestPage, detailPage, everyRect)
+            for everyRect in detailRect:
+                pageToLink = doc[detailPage]
+                # print('pageToLink', pageToLink)
+                print ('about to link', eachDestPage, detailPage, everyRect)
 
-                    # lnks = {'kind': 1, 'xref': 864, 'from': everyRect, 'type': 'goto', 'page': pages[0], 'to': fitz.Point(100.0, 200.0), 'zoom': 0.0}
-                    lnks = {'kind': 1, 'xref': 864, 'from': everyRect, 'type': 'goto', 'page': eachDestPage, 'to': fitz.Point(100.0, 200.0), 'zoom': 0.5}
-                    pageToLink.insert_link(lnks)
-                    page = doc.reload_page(pageToLink)
+                # lnks = {'kind': 1, 'xref': 864, 'from': everyRect, 'type': 'goto', 'page': pages[0], 'to': fitz.Point(100.0, 200.0), 'zoom': 0.0}
+                lnks = {'kind': 1, 'xref': 864, 'from': everyRect, 'type': 'goto', 'page': eachDestPage, 'to': fitz.Point(100.0, 200.0), 'zoom': 0.5}
+                pageToLink.insert_link(lnks)
+                page = doc.reload_page(pageToLink)
         
 
-
-# for each in detials:
-#     # print ('each', each)
-#     # testKey = [k for k, v in each.items()]
-#     detailPage = list(each.keys())[0]
-#     detailRect = list(each.values())[0]
-#     # print('detailPage',detailPage)
-#     if (detailPage != pages[0]):
-#         # print('detailRect', detailRect)
-#         lnks = doc[detailPage].links()
-#         # print('links', lnks)
-
-#         for everyRect in detailRect:
-#             pageToLink = doc[detailPage]
-#             # print('pageToLink', pageToLink)
-#             # print ('about to link',detailPage, everyRect)
-
-#             # lnks = {'kind': 1, 'xref': 864, 'from': everyRect, 'type': 'goto', 'page': pages[0], 'to': fitz.Point(100.0, 200.0), 'zoom': 0.0}
-#             lnks = {'kind': 1, 'xref': 864, 'from': everyRect, 'type': 'goto', 'page': detailPage, 'to': fitz.Point(100.0, 200.0), 'zoom': 0.5}
-#             pageToLink.insert_link(lnks)
-#             page = doc.reload_page(pageToLink)
-# # doc[2].doc[detpageNumber]
 
  
 doc.ez_save('pdfLinks.pdf')
 
+# --- spell check---------------------------------------------------------------
 
 misspelledList = []
 pageHeader = []
@@ -204,15 +188,40 @@ print("page text")
 for page in doc:
 # page = doc[0]
     ### SEARCH
-    # print('-----------page', page)
-    pageHeader.append(page)
+    print('-----------page', page)
+    pageHeader.append('page num {0}'.format(page.number))
     # textToCheck = page.get_text('text')
-    # --- spell check---------------------------------------------------------------
     textListClean =[]
     new_string = []
     updateMisspelled = []
-    textList = page.get_text('text').split('\n')
-    for each in textList:
+    textToIgnore = SearchText + excludeList
+    textToIgnoreList = []
+    textToIgnoreList2 = []
+    searchCharInit = ["$","@","&","Â","©",",", "ï","¿","|", "(", ")", "'",'½', 'ï', '�' ] 
+
+    pageText = page.get_text('text')
+    cleanedPageText = []
+    for eachtext in pageText:
+        for eachChar in searchCharInit:
+                if (eachChar not in eachtext):
+                    cleanedPageText.append(eachtext)
+
+    print(pageText)
+    # textList = page.get_text('text').split('\n')
+    textListMultiClean = []
+    for eachMultiStr in cleanedPageText:
+        splitText = eachMultiStr.split('\n')
+        if(len(splitText)>1):
+            for i in splitText:
+                textListMultiClean.append(i)
+        else:
+            textListMultiClean.append(splitText[0])
+
+    # print (textListMultiClean)
+    # textList = page.get_text('text').split('\n')
+
+    # for each in textList:
+    for each in textListMultiClean:
         # print (len(each))
         if (len(each) > 0):
             # print (each)
@@ -224,95 +233,90 @@ for page in doc:
                 # print (len(splitText))
                 textListClean.append(splitText[0])
 
-    for stringy in textListClean:
-        cleanstring = ''.join(char for char in stringy if char.isalnum())
+    # print (textListClean)
+    for textRemove in textListClean:
+    # for textRemove in textList:
+        print(textRemove)
 
+        if (textRemove not in textToIgnore):
+            # print (textRemove, textToIgnore)
+            textToIgnoreList.append(textRemove)
+
+    print (textToIgnoreList)
+    spell = SpellChecker(distance=1)
+    # misspelled = spell.unknown(textToIgnoreList)
+    misspelledLister = []
+    spezzed = spell.known(textToIgnoreList)
+    for eachword in textToIgnoreList:
+        if eachword not in spezzed:
+            misspelledLister.append(eachword)
+
+    # misspelled2 = list(misspelled)
+    print (misspelledLister)
+
+    # something about this is clumsy
+    # for stringy in misspelled:
+    for stringy in misspelled:
+        # cleanstring = ''.join(char for char in stringy if char.isalnum()) #I don't want to remove middle of word hyphnes etc, 
+        # print('stringy',stringy)
+        searchChar = ["$","@","&","Â","©",",", "ï","¿","|", "(", ")", "'",'½', 'ï'] 
+        cleanstring = ''
+        if(len(stringy)>1):
+            if (not stringy[len(stringy)-1].isalnum()):
+                # print (stringy, stringy[-1:], stringy[:-1])
+                cleanstring = stringy[:-1]
+                # cleanstring = ''.join(char for char in stringy if char.isalnum())
+                # cleanstring = re.sub("[$@&Â©,n22Â©ak]","",stringy)
+            elif (not stringy[0].isalnum()):
+                # print(stringy, stringy[1], stringy[1].isalnum(),  stringy[1:])
+                cleanstring = stringy[1:]
+            
+            else:
+                # print(stringy)
+                cleanstring = stringy
+
+        if (r'/' in stringy):
+            # break
+            # print('slash', stringy)
+            cleanstring = ''
+        for eachChar in searchChar:
+            if (eachChar in cleanstring):
+                cleanstring = ''
+        if(len(cleanstring)>1):
+
+            # print(cleanstring, ''.join(cleanstring.split('-')))
+            try:
+                if (int(''.join(cleanstring.split('-')))):
+                    # break
+                    # print('is_integer', cleanstring)
+                    cleanstring = ''
+            except:
+                # print('notstring')
+                x=1
         #remove exceptions 
-        if len(cleanstring) > 1:
-            new_string.append(''.join(char for char in stringy if char.isalnum()))
+        if len(cleanstring) > 3:
+            # print (cleanstring)
+            # new_string.append(''.join(char for char in stringy if char.isalnum()))
+            new_string.append(cleanstring)
+            # new_string.append(stringy)
 
-    spell = SpellChecker()
-    misspelled = spell.unknown(new_string)
+    for textRemove2 in new_string:
+        if textRemove2.lower() not in textToIgnore:
+            textToIgnoreList2.append(textRemove2)
 
-    misspelledList.append(misspelled)
+    misspelledcleaned = spell.unknown(textToIgnoreList2)
+
+    # misspelled = spell.unknown(textToIgnoreList)
+    # enumerate_list = [(index, element) for index, element in enumerate(misspelled)]
+    misspelledList.append(list(misspelledcleaned))
+    # misspelledList.append(new_string)
+    # misspelledList.append(misspelled)
 
     # print (misspelled)
 
+# print('report.csv', pageHeader, zip(*misspelledList) )
+# writeCsv('report.csv', pageHeader, zip(*misspelledList)  )
+writeList = list(map(list, itertools.zip_longest(*misspelledList, fillvalue=None)))
+# print('report.csv', pageHeader, writeList)
+writeCsv(pdfLink[:-4]+'_report.csv', pageHeader, writeList )
 
-
-writeCsv('report.csv', pageHeader, misspelledList )
-
-
-
-
-    # --- linking---------------------------------------------------------------
-    # --earlier pages link to later pages
-    # -- can use input list of details
-
-    # text options
-    # print('text', page.get_text('text')) # gets words only
-    # print('blocks', page.get_text('blocks')) # Yikes - too much, seems well contained but a mess
-    # print('words', page.get_text('words')) # gets list of word and rect xy, xy
-    #print('json', page.get_text('json')) # gets structured items with word and bbox xy, xy
-    # print('xml', page.get_text('xml')) #formating only
-    
-
-    myJSON =  json.loads('{0}'.format(page.get_text('json')))
-    # print(myJSON['lines'])
-    # print(myJSON['blocks'])
-    jsonData = myJSON['blocks']
-    for x in jsonData:
-            # keys = x.keys()
-            # print(keys)
-            # values = x.values()
-            # print(values)
-            if ('lines' in x.keys()):
-                # print(x['lines'][0]['spans'][0]['text'])
-                values = x['lines']
-            
-            # for ix in values:
-            #     # print(ix)
-            #     if type(ix) != int:
-            #         if len(ix)>1:
-            #             keysix = ix.keys()
-            #             # print(keysix)
-            #     # valuesix = ix.values()
-            #     # print(valuesix)
-    
-
-
-
-    text = "DET Z100"
-    text_instances = page.search_for(text)
-    if (len(text_instances) > 1):
-        print (text_instances)
-        pdf_writer.addLink(
-            pagenum=0,  # index of the page on which to place the link
-            pagedest=1,  # index of the page to which the link should go
-            # clickable area x1, y1, x2, y2 (starts bottom left corner)
-            rect=RectangleObject([(text_instances.x0), (y2-text_instances.y0), (text_instances.x1), (y2-text_instances.y1)]),
-            # border
-            # fit
-
-    )
-
-# # add each page in pdf to pdf writer
-# num_of_pages = pdf_reader.getNumPages()
-
-# for page in range(num_of_pages):
-#     current_page = pdf_reader.getPage(page)
-#     pdf_writer.addPage(current_page)
-# print(f'{(770), (y2-104), (800), (y2-130)}')
-# # Add Link
-# pdf_writer.addLink(
-#     pagenum=0,  # index of the page on which to place the link
-#     pagedest=1,  # index of the page to which the link should go
-#     # clickable area x1, y1, x2, y2 (starts bottom left corner)
-#     rect=RectangleObject([(570), (y2-550), (650), (y2-600)]),
-#     # border
-#     # fit
-
-# )
-
-# with open(path.abspath('pdf_with_link.pdf'), 'wb') as link_pdf:
-#     pdf_writer.write(link_pdf)
